a7ebf9ee93c168ad036250fea291d83a5b6b0e1b
Edward Z. Yang <ezyang@meta.com>
2025-07-24 19:06:11 -0700

    Add aot_export_joint_with_descriptors and aot_compile_joint_with_descriptors
    Signed-off-by: Edward Z. Yang <ezyang@meta.com>
    ghstack-source-id: ba21fa08340b5a195fbee666e5c5a6fe3dc36f62
    Pull-Request: https://github.com/pytorch/pytorch/pull/158715

--- torch/_functorch/_aot_autograd/schemas.py
+++ torch/_functorch/_aot_autograd/schemas.py
@@ -1196,6 +1196,10 @@ class AOTGraphCapture:  # Produced by aot_stage1_graph_capture
     # has a specific calling convention that can be relied upon by external
     # callers.  In other situations, the calling convention is unspecified and
     # only aot_stage2_compile knows how to deal with them.
+    #
+    # TODO: don't call this graph, it's a GraphModule
+    # TODO: should we return a GraphModule here??  More convenient for
+    # printing, I guess?
     graph: torch.fx.GraphModule
 
     # When compiling with autograd support, this is the joint_inputs, which is
@@ -1279,3 +1283,25 @@ class JointTraceFn(Protocol):
         tuple[list[FxValue], list[Optional[Tensor]]],
         tuple[list[AOTOutput], list[Optional[AOTOutput]]],
     ]: ...
+
+
+@dataclass
+class JointWithDescriptors:
+    _aot_state: AOTState
+    _aot_graph_capture: AOTGraphCapture
+
+    # The exact order parameters and buffers are expected to be passed into
+    # the final compiled function.  Parameters before buffers.
+    params_spec: list[str]
+    buffers_spec: list[str]
+
+    in_spec: pytree.TreeSpec
+    out_spec: pytree.TreeSpec
+
+    @property
+    def graph_module(self):
+        return self._aot_graph_capture.graph
+
+    @graph_module.setter
+    def graph_module(self, value):
+        self._aot_graph_capture.graph = value
